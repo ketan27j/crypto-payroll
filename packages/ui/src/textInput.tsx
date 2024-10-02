8eff4a41466d3cf39392b51fa4fb187786f4ce01
alokj10 <alok_j10@yahoo.co.in>
2024-10-03 00:10:07 +0530

    resolve conflict

--- packages/ui/src/textInput.tsx
+++ packages/ui/src/textInput.tsx
@@ -5,18 +5,16 @@ export const TextInput = ({
     onChange,
     label,
     value,
-    type = "text",
-    readOnly = false
+    type = "text"
 }: {
     placeholder: string;
     onChange: (value: string) => void;
     label: string;
     value: string | number;
     type?: string;
-    readOnly: boolean;
 }) => {
     return <div className="pt-2">
         <label className="block mb-2 text-sm font-medium text-gray-900">{label}</label>
-        <input readOnly={readOnly} onChange={(e) => onChange(e.target.value)} value={value} type={type} className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} />
+        <input onChange={(e) => onChange(e.target.value)} value={value} type={type} className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} />
     </div>
 }
