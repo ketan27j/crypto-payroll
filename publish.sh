c64f30abf28d8db430971aa7e1f3b8e294ef89cc
alokj10 <alok_j10@yahoo.co.in>
2024-10-11 18:48:35 +0530

    update publish script

--- publish.sh
+++ publish.sh
@@ -2,17 +2,17 @@ cd /projects/crypto-function
 
 git pull
 
-container_id='docker ps -a --filter "name=crypto-function" --format "{{.ID}}"'
+container_id=`docker ps -a --filter "name=crypto-function" --format "{{.ID}}"`
+echo 'containerId:' $container_id
+docker stop $container_id
 
-docker stop container_id
+docker container prune -f
 
-docker container prune -y
+image_id=`docker images --format="{{.Repository}} {{.ID}}" | grep "crypto-function" | cut -d " " -f2`
+echo 'imageId:' $image_id
+docker rmi $image_id
 
-image_id='docker images --format="{{.Repository}} {{.ID}}" | grep "crypto-function" | cut -d " " -f2'
-
-docker rmi image_id
-
-docker image prune -y
+docker image prune -f
 
 docker build -t crypto-function .
 
